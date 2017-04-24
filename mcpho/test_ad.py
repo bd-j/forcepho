fix bug in lnlike_grad calculation, optimization working.

mcpho/test_ad.py
@@ -143,8 +143,10 @@ class Likelihood(object):
     def lnlike(self, params):
         self.source.update_vec(params)
         image = self.model.counts_and_gradients(self.source)
-        lnlike = -0.5 * np.sum(((self.data - image[0, :]) / self.unc)**2)
-        lnlike_grad = np.sum((self.data - image[1:, :]) / self.unc**2, axis=-1)
+        delta = self.data - image[0, :]
+        chi = delta / self.unc
+        lnlike = -0.5 * np.sum(chi**2)
+        lnlike_grad = np.sum(chi / self.unc * image[1:, :] , axis=-1)
         return lnlike, lnlike_grad
 
 
@@ -241,39 +243,70 @@ if __name__ == "__main__":
     lnp = pixel.counts(ptrue)
     print(pixel.counts_and_gradients(galaxy)[1])
 
-    galaxy = Source(a=a, b=b, theta=theta, x0=x0, y0=y0, nx=70, ny=50)
+
+    # --- Set up the galaxy and pixels -----
+    galaxy = Source(a=a, b=b, theta=theta, x0=x0, y0=y0, nx=50, ny=70, n=1.0)
     nx = ny = 40
     pixel_list = [PixelResponse(mu=[i, j]) for i in range(-nx/2, nx/2) for j in range(-ny/2, ny/2)]
     pixels = ImageModel(pixel_list)
 
+    import sys
+    #sys.exit()
 
+    # ---- Fake image -----
     image = pixels.counts(galaxy)
     unc = np.sqrt(image)
+    coo_true = galaxy.coordinates(galaxy.params)
 
+    # ---- Likelihood object and negative ln-likelihood for minimization
     model = Likelihood(pixels, galaxy, image, unc)
-
     def nll(params):
         v, g = model.lnlike(params)
         return -v, -g
 
+    # --- Initial parameter value -----
     p0 = np.array([11.0, 5.1, 0.3, 0.3, -0.3])
-    coo_true = galaxy.coordinates(ptrue)
     coo_start = galaxy.coordinates(p0)
 
+    # ---- Optimization ------
     bounds = [(0, 100), (0., 100), (0, np.pi), (-10, 10), (-10, 10)]
-    
     from scipy.optimize import minimize
-    r = minimize(nll, p0, jac=True, bounds=bounds)
+    result = minimize(nll, p0, jac=True, bounds=bounds)
+    pf = result.x
+    coo_f = galaxy.coordinates(pf)
 
-    galaxy.update_vec(ptrue)
-    imgr = pixels.counts_and_gradients(galaxy)
+    # Plot image gradients
+
+    def plot_gradients(parvec, galaxy, pixels):
+        galaxy.update_vec(parvec)
+        imgr = pixels.counts_and_gradients(galaxy)
+
+        import matplotlib.pyplot as pl
+        from mpl_toolkits.axes_grid1 import make_axes_locatable
+        parn = ['counts',
+                '$\partial counts/ \partial a$','$\partial counts / \partial b$',
+                r'$\partial counts/ \partial \theta$',
+                '$\partial counts / \partial x0$', '$\partial counts / \partial y0$']
+        fig, axes = pl.subplots(2, 3, figsize=(20, 11))
+        for i, ax in enumerate(axes.flat):
+            c = ax.imshow(imgr[i, :].reshape(nx, ny).T, origin='lower')
+            div = make_axes_locatable(ax)
+            cax = div.append_axes("right", size="10%", pad=0.05)
+            cbar = pl.colorbar(c, cax=cax)
+            ax.set_title(parn[i])
+        return fig
+
+
+    fig_true = plot_gradients(ptrue)
+    fig_fit = plot_gradients(pf)
     
-    import matplotlib.pyplot as pl
-    from mpl_toolkits.axes_grid1 import make_axes_locatable
-    parn = ['counts',
-            '$\partial counts/ \partial a$','$\partial counts / \partial b$',
-            r'$\partial counts/ \partial \theta$',
-            '$\partial counts / \partial x0$', '$\partial counts / \partial y0$']
+            
+    # -- Plot likelihood gradients ------
+    sys.exit()
+    parn = [r'residual ($\Delta$)',
+            r'$\partial \Delta/ \partial a$',r'$\partial \Delta / \partial b$',
+            r'$\partial \Delta/ \partial \theta$',
+            r'$\partial \Delta/ \partial x0$', r'$\partial \Delta / \partial y0$']
     fig, axes = pl.subplots(2, 3, figsize=(20, 11))
     for i, ax in enumerate(axes.flat):
         c = ax.imshow(imgr[i, :].reshape(nx, ny).T, origin='lower')