998f8f5ef33e92de2a324e149a5016870d7797e0
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2017-05-06 23:17:03 -0400

    better layout for optimization with smoothed gaussians

--- forcepho/approximate_gaussian.py
+++ forcepho/approximate_gaussian.py
@@ -4,14 +4,17 @@ from scipy.special import factorial
 import numpy as np
 from functools import partial
 
+import matplotlib.pyplot as pl
+from matplotlib.backends.backend_pdf import PdfPages
+import h5py
 
 from scipy.special import gamma, gammaincinv
 from scipy.special import hyp1f1 as hyper
-
 from scipy.optimize import minimize
 
 x = np.linspace(1e-3, 8.0, int(1e3))
-x = np.arange(0.0005, 8.0, 0.001)
+x = np.arange(0.0005, 10.0, 0.001)
+
 
 def fit_gaussians(nsersic=4., rh=1., smoothing=0.0, radii=None,
                   ncomp=6, x=x):
@@ -28,8 +31,8 @@ def fit_gaussians(nsersic=4., rh=1., smoothing=0.0, radii=None,
         amp = np.linspace(np.log(0.01), np.log(maxrad), len(radii))
         p0 = amp
 
-    extras = {'nsersic': nsersic, 'rh': rh, 'smoothing': smoothing,
-              'radii': radii, 'x': x}
+    sersic = sersic_profile(x, n=nsersic, rh=rh, sigma=smoothing)
+    extras = {'radii': radii, 'x': x, 'target': sersic}
     partial_chisq = partial(chisq, **extras)
     bounds = npar * [(0, None)]
 
@@ -39,34 +42,33 @@ def fit_gaussians(nsersic=4., rh=1., smoothing=0.0, radii=None,
     return result
 
 
-def chisq(params, nsersic=4, rh=1, smoothing=0.0, radii=None,
-          x=np.linspace(1e-3, 8.0, int(1e3)), return_models=False):
+def chisq(params, x=None, target=None, radii=None,
+          return_models=False):
     """
     :param params:
         ln of the parameters describing the amplitudes and sigmas of the
         gaussian mixture
     """
-
-    sersic = sersic_profile(x, n=nsersic, rh=rh, sigma=smoothing)
     if radii is not None:
         disps = np.exp(radii)
         amps = np.exp(params[:len(radii)])
     else:
         amps = np.exp(params[:len(params)/2])
         disps = np.exp(params[len(params)/2:])
-    amps /= disps * np.sqrt(2 * np.pi)
+    # this is to account for two-d normalization and smoothing
     disps = np.hypot(disps, smoothing)
+    amps /= disps * np.sqrt(2 * np.pi)
     gauss = normal_oned(x, 0.0, amps, disps)
-    delta = gauss - sersic
+    delta = gauss - target
     weighted_chisq = np.sum(delta * delta * x) / np.sum(x) #+ 1e-3 * np.sum(disps**2)
     weighted_chisq *= 1e-2
     if return_models:
-        return weighted_chisq, x, sersic, gauss
+        return weighted_chisq, x, target, gauss
     else:
         return  weighted_chisq 
 
 
-def sersic_profile(x, n=4, rh=1, sigma=0.0, order=100):
+def sersic_profile(x, n=4, rh=1, sigma=0.0, order=50):
     """Calculate a sersic profile, optionally with some small smoothing
     """
     alpha = gammaincinv(2 * n, 0.5)
@@ -76,11 +78,13 @@ def sersic_profile(x, n=4, rh=1, sigma=0.0, order=100):
         return np.exp(-(x / r0)**(1.0 / n) + alpha)
     else:
         # xx = np.concatenate([x, np.atleast_1d(rh)])
-        xx = x
-        print(sigma, r0, rh, n)
+        value = np.zeros_like(x)
+        use = x < sigma * 20
+        xx = x[use]
+        #print(sigma, r0, rh, n)
         p = -0.5 * (xx / sigma)**2
         A = np.exp(p)
-        total = 0
+        total = np.zeros_like(xx)
         for k in range(order):
             k = k * 1.0
             mu = 1 + k/(2*n)
@@ -89,7 +93,12 @@ def sersic_profile(x, n=4, rh=1, sigma=0.0, order=100):
             term *= gamma(mu)
             term *= hyper(mu, 1, -p)
             total += term
-        value = A * total * np.exp(alpha)
+            #good = np.isfinite(term)
+            #total[good] += term[good]
+        value[use] = A * total * np.exp(alpha) # last term normalizes to unit intensity at the half-light radius
+        # deal with large radii where the gaussian fails, by replacing with the unconvolved sersic
+        bad = (~np.isfinite(value)) | (~use)
+        value[bad] = np.exp(-(x[bad] / r0)**(1.0 / n) + alpha)
         return value #[:-1] / value[-1], value[-1]
 
 
@@ -119,33 +128,79 @@ def normal(x, sigma):
 
 if __name__ == "__main__":
 
-    # Set up gaussians in pixel space
-    minrad, maxrad, dlnr = 0.001, 4.0, np.log(2)
-    lnradii = np.arange(np.log(minrad), np.log(maxrad), dlnr)
-    radii = np.exp(lnradii)
+    # Decide whether to fit smoothed sersic profiles (smoothing > 0)
+    smoothing = 0.1 # in units of pixels
+    outname = "mog_model.smooth={:2.1f}.h5".format(smoothing)
+    # set the pixel array that will be used for calculating models
+    x = np.arange(0.0005, 10.0, 0.001)
     
+    # Set up gaussians in pixel space
+    minrad, maxrad, dlnr = 0.005, 6.0, np.log(2)
+    lnradii = np.arange(np.log(minrad), np.log(maxrad), dlnr)
+    if smoothing > 0:
+        lnradii = np.insert(lnradii, 0, -np.inf)
+    radii = np.exp(lnradii)
+
     # Grid in nsersic and rh
-    rgrid = np.arange(0.25, 4.25, 0.25)
-    ngrid = np.arange(1.0, 5.0, 0.5)
+    rgrid = np.arange(0.5, 4.25, 0.5)
+    ngrid = np.arange(1.0, 5.5, 1.0)
 
     nres = len(ngrid) * len(rgrid)
-    dt = np.dtype([('nsersic', np.float), ('rh', np.float), ('chisq', np.float),
-                   ('amplitudes', np.float, (len(radii),)),
-                   ('truth', np.float, (len(x),)),
-                   ('model', np.float, (len(x),))])
-                   
-    result = np.zeros(nres, dtype=dt)
-    
-    # 
+#    dt = np.dtype([('nsersic', np.float), ('rh', np.float), ('chisq', np.float),
+#                   ('amplitudes', np.float, (len(radii),)),
+#                   ('sersic', np.float, (len(x),)),
+#                   ('model', np.float, (len(x),)),
+#                   ('smooth_sersic', np.float, (len(x),))])
+#    result = np.zeros(nres, dtype=dt)
+
+    result = h5py.File(outname, 'w')
+    result.create_dataset('radii', data=radii)
+    result.create_dataset('x', data=x)
+    result.attrs['smoothing'] = smoothing
+    result.create_dataset('nsersic', shape=(nres,))
+    result.create_dataset('rh', shape=(nres,))
+    result.create_dataset('chisq', shape=(nres,))
+    result.create_dataset('amplitudes', shape=(nres, len(radii)))
+    result.create_dataset('truth', shape=(nres, len(x)))
+    result.create_dataset('model', shape=(nres, len(x)))
+    result.create_dataset('smooth_sersic', shape=(nres, len(x)))
+
+    pdf = PdfPages('mog_model.pdf')
+    # Loop over radii and sersic indices
     for i, (ns, rh) in enumerate(product(ngrid, rgrid)):
         print(ns, rh)
-        x = np.arange(0.0005, rh * 4, 0.001)
-        res = fit_gaussians(nsersic=ns, rh=rh, radii=lnradii, x=x)
-        result[i]['nsersic'] = ns
-        result[i]['rh'] = rh
-        result[i]['amplitudes'] = res.x
-        result[i]['chisq'] = res.fun
-        chi, xx, sersic, gauss = chisq(res.x, nsersic=ns, rh=rh, radii=lnradii, x=x,
+        xx = x[x < max(rh * 4, 1.1* radii.max())]
+        res = fit_gaussians(nsersic=ns, rh=rh, radii=lnradii,
+                            x=xx, smoothing=smoothing)
+        result['nsersic'][i] = ns
+        result['rh'][i] = rh
+        result['amplitudes'][i] = np.exp(res.x)
+        result['chisq'][i] = res.fun
+        sersic = sersic_profile(x, n=ns, rh=rh, sigma=0)
+        sm_sersic = sersic_profile(x, n=ns, rh=rh, sigma=smoothing)
+        _, _, sm_sersic, gauss = chisq(res.x, radii=lnradii,
+                                       x=x, target=sm_sersic,
                                        return_models=True)
-        result[i]['truth'] = sersic
-        result[i]['model'] = gauss
+        result['truth'][i] = sersic
+        result['smooth_sersic'][i] = sm_sersic
+        result['model'][i] = gauss
+        result.flush()
+
+        fig, ax = pl.subplots()
+        ax.plot(x, sersic, label='Sersic:n={:3.1f}, rh={:3.2f}'.format(ns, rh))
+        if smoothing > 0:
+            ax.plot(x, sm_sersic, label='smooth Sersic')
+        ax.plot(x, gauss, label='GaussMix')
+        ax.axvline(rh * 4, linestyle=":", color='k')
+        ax.set_xlabel('pixels')
+        ax.set_title('chisq={}'.format(res.fun))
+        ax.set_xscale('log')
+        ax.set_yscale('log')
+        ax.set_ylim(1e-6, 1e5)
+        ax.set_xlim(1e-3, x.max())
+        ax.legend()
+        pdf.savefig(fig)
+        pl.close(fig)
+
+    pdf.close()
+    result.close()
