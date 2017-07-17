81f53401609e13a3c2ca48e015c0bb31bd94e1ac
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2017-07-17 12:09:22 -0400

    rename some variables, change array shapes a bit in proto

--- forcepho/proto/proto.py
+++ forcepho/proto/proto.py
@@ -1,5 +1,6 @@
 # Some code along the lines of what we will do in C++
 
+
 import numpy as np
 
 __all__ = ["ImageGaussian", "Star", "Galaxy", "GaussianImageGalaxy",
@@ -27,25 +28,26 @@ class ImageGaussian(object):
     fxy = 0.
     fyy = 0.
 
-    # derivs = [dA_dpsi, dA_dq, dA_dphi, dA_dn, dA_dr, D.flatten(), dF_dq.flat[inds], dF_dphi.flat[inds]]
-    derivs = None  # this is the dGaussian_dScene Jacobian matrix, in a sparse format
+    # derivs = [dA_dflux, dA_dq, dA_dpa, dA_dsersic, dA_drh, D.flatten(), dF_dq.flat[inds], dF_dpa.flat[inds]]
+    derivs = None  # this is the dGaussian_dScene Jacobian matrix, possibly in a sparse format
     #float dGaussian_dScene[NDERIV];
     
 
 class Star(object):
 
     id = 0
-    psi = 0. # flux
-    ra = 0.
-    dec = 0.
-    q = 1.
-    phi = 0.
-    n = 0.
-    r = 0.
     fixed = False
     ngauss = 1
     radii = np.zeros(0)
 
+    # Parameters
+    flux = 0.     # flux
+    ra = 0.
+    dec = 0.
+    q = 1.        # axis ratio
+    pa = 0.       # postion angle (N of E)
+    sersic = 0.   # sersic index
+    rh = 0.       # half light radius
 
     def __init__(self):
         pass
@@ -60,13 +62,12 @@ class Star(object):
 
     @property
     def amplitudes(self):
-        """Code here for getting amplitudes from a splined look-up table
-        (dependent on self.n and self.r)
+        """
         """
         return np.ones(1)
 
     @property
-    def damplitude_dn(self):
+    def damplitude_dsersic(self):
         """Code here for getting amplitude derivatives from a splined look-up
         table (dependent on self.n and self.r)
         """
@@ -74,7 +75,7 @@ class Star(object):
         return np.zeros(1)
 
     @property
-    def damplitude_dr(self):
+    def damplitude_drh(self):
         """Code here for getting amplitude derivatives from a splined look-up
         table (dependent on self.n and self.r)
         """
@@ -85,48 +86,50 @@ class Star(object):
 class Galaxy(object):
     """Parameters describing a galaxy in the celestial plane (i.e. the Scene parameters)
     For each galaxy there are 7 parameters.
-    * psi: total flux
+    * flux: total flux
     * ra: right ascension (degrees)
     * dec: declination (degrees)
-    * q, phi: axis ratio and position angle (might be parameterized differently)
+    * q, pa: axis ratio and position angle (might be parameterized differently)
     * n: sersic index
     * r: half-light radius (arcsec)
     
     """
     id = 0
-    psi = 0. # flux
-    ra = 0.
-    dec = 0.
-    q = 1.  # axis ratio
-    phi = 0.  # position angle (N of E)
-    n = 0.  # sersic index
-    r = 0.  # half-light radius (arcsec)
     fixed = False
     ngauss = 10
     radii = np.arange(ngauss)
+
+    # Parameters
+    flux = 0.     # flux
+    ra = 0.
+    dec = 0.
+    q = 1.        # axis ratio
+    pa = 0.       # postion angle (N of E)
+    sersic = 0.   # sersic index
+    rh = 0.       # half light radius
 
     @property
     def amplitudes(self):
         """Code here for getting amplitudes from a splined look-up table
         (dependent on self.n and self.r)
         """
-        return amps
+        return np.array([0.5, 0.5])
 
     @property
-    def damplitude_dn(self):
+    def damplitude_dsersic(self):
         """Code here for getting amplitude derivatives from a splined look-up
         table (dependent on self.n and self.r)
         """
-        # ngauss array of da/dn
-        return da_dn
+        # ngauss array of da/dsersic
+        return np.zeros(self.ngauss)
 
     @property
-    def damplitude_dr(self):
+    def damplitude_drh(self):
         """Code here for getting amplitude derivatives from a splined look-up
         table (dependent on self.n and self.r)
         """
-        # ngauss array of da/dr
-        return da_dr
+        # ngauss array of da/drh
+        return np.zeros(self.ngauss)
 
     
     @property
@@ -205,7 +208,7 @@ def convert_to_gaussians(galaxy, stamp):
     """
     # Get the transformation matrix
     D = stamp.distortion
-    R = rotation_matrix(galaxy.phi)
+    R = rotation_matrix(galaxy.pa)
     S = scale_matrix(galaxy.q)
     T = np.dot(D, np.dot(R, S))
 
@@ -237,9 +240,10 @@ def convert_to_gaussians(galaxy, stamp):
             gauss.fyy = f[1, 1]
 
             # Now get centers and amplitudes
+            # TODO: Add gain/conversion from stamp to go from physical flux to counts.
             gauss.xcen, gauss.ycen = gmean + stamp.psf.means[j]
             am, al = galaxy.amplitudes[i], stamp.psf.amplitudes[j]
-            gauss.amp = galaxy.psi * am * al * np.linalg.det(f)**(0.5) / (2 * np.pi)
+            gauss.amp = galaxy.flux * am * al * np.linalg.det(f)**(0.5) / (2 * np.pi)
 
             # And add to list of gaussians
             gig.gaussians[i, j] = gauss
@@ -250,15 +254,15 @@ def convert_to_gaussians(galaxy, stamp):
 def get_gaussian_gradients(galaxy, stamp, gig):
     # Get the transformation matrix
     D = stamp.distortion
-    R = rotation_matrix(galaxy.phi)
+    R = rotation_matrix(galaxy.pa)
     S = scale_matrix(galaxy.q)
     T = np.dot(D, np.dot(R, S))
 
     # And its deriatives with respect to scene parameters
     dS_dq = scale_matrix_deriv(galaxy.q)
-    dR_dphi = rotation_matrix_deriv(galaxy.phi)
+    dR_dpa = rotation_matrix_deriv(galaxy.pa)
     dT_dq = np.dot(D, np.dot(R, dS_dq))
-    dT_dphi = np.dot(D, np.dot(dR_dphi, S))
+    dT_dpa = np.dot(D, np.dot(dR_dpa, S))
 
     for i in range(galaxy.ngauss):
         gcovar = galaxy.covariances[i]
@@ -271,30 +275,42 @@ def get_gaussian_gradients(galaxy, stamp, gig):
             F = np.linalg.inv(Sigma)
             detF = np.linalg.det(F)
             am, al = galaxy.amplitudes[i], stamp.psf.amplitudes[j]
-            K = galaxy.psi * am * al * detF**(0.5) / (2 * np.pi)
+            K = galaxy.flux * am * al * detF**(0.5) / (2 * np.pi)
             
             # Now get derivatives
             # Of F
-            dSigma_dq = np.matmul(T, np.matmul(gcovar, dT_dq.T)) + np.matmul(dT_dq, np.matmul(gcovar, T.T))
-            dSigma_dphi = np.matmul(T, np.matmul(gcovar, dT_dphi.T)) + np.matmul(dT_dphi, np.matmul(gcovar, T.T))
+            dSigma_dq = (np.matmul(T, np.matmul(gcovar, dT_dq.T)) +
+                         np.matmul(dT_dq, np.matmul(gcovar, T.T)))
+            dSigma_dpa = (np.matmul(T, np.matmul(gcovar, dT_dpa.T)) +
+                          np.matmul(dT_dpa, np.matmul(gcovar, T.T)))
             dF_dq = -np.matmul(F, np.matmul(dSigma_dq, F))  # 3
-            dF_dphi = -np.matmul(F, np.matmul(dSigma_dphi, F))  # 3
+            dF_dpa = -np.matmul(F, np.matmul(dSigma_dpa, F))  # 3
             ddetF_dq = detF * np.trace(np.matmul(Sigma, dF_dq))
-            ddetF_dphi = detF * np.trace(np.matmul(Sigma, dF_dphi))
+            ddetF_dpa = detF * np.trace(np.matmul(Sigma, dF_dpa))
             # of Amplitude
+            # TODO: Add gain/conversion from stamp to go from physical flux to counts
             dA_dq = K / (2 * detF) * ddetF_dq  # 1
-            dA_dphi = K / (2 * detF) * ddetF_dphi  # 1
-            dA_dpsi = K / galaxy.psi # 1
-            dA_dn = K / am * galaxy.damplitude_dn[i] # 1
-            dA_dr = K / am * galaxy.damplitude_dr[i] # 1
+            dA_dpa = K / (2 * detF) * ddetF_dpa  # 1
+            dA_dflux = K / galaxy.flux # 1
+            dA_dsersic = K / am * galaxy.damplitude_dsersic[i] # 1
+            dA_drh = K / am * galaxy.damplitude_drh[i] # 1
 
-            # And derivatives to gaussians
+            # Add derivatives to gaussians
+            # As a list of just nonzero
             inds = [0, 1, 3] # slice into a flattened symmetric matrix to get unique components
-            derivs = ([dA_dpsi, dA_dq, dA_dphi, dA_dn, dA_dr] + 
+            derivs = ([dA_dflux, dA_dq, dA_dpa, dA_dsersic, dA_drh] + 
                        D.flatten().tolist() +
                        dF_dq.flat[inds].tolist() +
-                       dF_dphi.flat[inds].tolist())
-            derivs = np.array(derivs)
+                       dF_dpa.flat[inds].tolist())
+            # as the 7 x 6 jacobian matrix
+            jac = [[dA_dflux, 0, 0, 0, 0, 0],  # d/dFlux
+                   [0, D[0, 0], D[1, 0], 0, 0, 0],  # d/dAlpha
+                   [0, D[0, 1], D[1, 1], 0, 0, 0],  # d/dDelta
+                   [dA_dq, 0, 0, dF_dq[0,0], dF_dq[1,0], dF_dq[1,1]],  # d/dQ
+                   [dA_dpa, 0, 0, dF_dpa[0,0], dF_dpa[1,0], dF_dpa[1,1]],  # d/dPA
+                   [dA_dsersic, 0, 0, 0, 0, 0],  # d/dSersic
+                   [dA_drh, 0, 0, 0, 0, 0]]  # d/dRh
+            derivs = np.array(jac)
             gig.gaussians[i, j].derivs = derivs
 
     return gig
@@ -320,7 +336,7 @@ def compute_gaussian(g, xpix, ypix, second_order=True, compute_deriv=True):
 
     :returns grad:
         The gradient of the counts with respect to the 6 parameters of the
-        gaussian in image coordinates
+        gaussian in image coordinates, ndarray of shape (nderiv, npix)
     """
 
     # All transformations should keep the determinant of the covariance matrix
@@ -354,7 +370,7 @@ def compute_gaussian(g, xpix, ypix, second_order=True, compute_deriv=True):
     dC_dfy = -0.5*C*dy*dy - second_order * c_h * (1. + 2.*dy*vy) / 24.
     dC_dfxy = -1.0*C*dx*dy - second_order * c_h * (1. + 2.*dy*vy) / 24.
 
-    return np.array(C), np.array([dC_dA, dC_dx, dC_dy, dC_dfx, dC_dfy, dC_dfxy]).T
+    return np.array(C), np.array([dC_dA, dC_dx, dC_dy, dC_dfx, dC_dfy, dC_dfxy])
 
 
 def scale_matrix(q):
