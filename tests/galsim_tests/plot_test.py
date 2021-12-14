160efac51e63eb5786fdbc8d419c32b9a49cfbf3
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2021-12-13 21:46:38 -0500

    add plotting module, sersic mixtures.

---
+++ tests/galsim_tests/plot_test.py
@@ -0,0 +1,248 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import glob, os, sys
+import argparse
+from copy import deepcopy
+
+import numpy as np
+import matplotlib.pyplot as pl
+from matplotlib.ticker import FormatStrFormatter
+
+from astropy.io import fits
+from forcepho.postprocess import Samples, Residuals, postsample_catalog
+
+fsize = 8, 9.5
+
+
+def plot_trace(patchname, title_fmt=".2g", fsize=fsize):
+    samples = Samples(patchname)
+    fig, axes = pl.subplots(7, sharex=True, figsize=fsize)
+    samples.show_chain(0, axes=np.array(axes), truth=samples.active[0])
+    for i, c in enumerate(samples.bands + samples.shape_cols):
+        ax = axes[i]
+        xx = samples.chaincat[0][c]
+        truth = samples.active[c][0]
+        lim = np.percentile(xx, [1, 99])
+        ax.set_ylim(*lim)
+        v = np.percentile(xx, [16, 50, 84])
+        qm, qp = np.diff(v)
+        p = np.max(np.ceil(np.abs(np.log10(np.diff(v))))) + 1
+        # could do better here about automating the format
+        cfmt = "{{:.{}g}}".format(int(p)).format
+        fmt = "{{0:{0}}}".format(title_fmt).format
+        title = r"${{{0}}}_{{-{1}}}^{{+{2}}}$"
+        title = title.format(cfmt(v[1]), fmt(qm), fmt(qp))
+        ax.text(1.0, 0.7, title, color="blue", transform=ax.transAxes)
+        ax.text(1.0, 0.2, cfmt(truth), color="red", transform=ax.transAxes)
+
+    axes[-1].set_xlabel("HMC iteration")
+
+    return fig, axes
+
+
+def plot_corner(patchname, smooth=0.05, hkwargs=dict(alpha=0.65),
+                dkwargs=dict(color="red", marker="."), fsize=(8, 8)):
+    from prospect.plotting.corner import allcorner, scatter
+    samples = Samples(patchname)
+    truth = np.atleast_2d(samples.starting_position)
+    labels = samples.chaincat.dtype.names[1:]
+
+    fig, axes = pl.subplots(7, 7, figsize=fsize)
+    axes = allcorner(samples.chain.T, labels, axes,
+                     color="royalblue",  # qcolor="black",
+                     psamples=truth.T,
+                     smooth=smooth, hist_kwargs=hkwargs,
+                     samples_kwargs=dkwargs)
+    for i, ax in enumerate(np.diag(axes)):
+        ax.axvline(truth[0, i], color="red")
+        if (labels[i] == "ra") | (labels[i] == "dec"):
+            axes[i, 0].yaxis.set_major_formatter(FormatStrFormatter('%.2g'))
+            axes[-1, i].xaxis.set_major_formatter(FormatStrFormatter('%.2g'))
+
+    # this doesn't do anything
+    #[ax.set_xlabel(ax.get_xlabel(), labelpad=200) for ax in axes[-1,:]]
+    #[ax.set_ylabel(ax.get_ylabel(), labelpad=30) for ax in axes[:, 0]]
+
+    return fig, axes
+
+
+def plot_residual(patchname):
+    s = Samples(patchname)
+    r = Residuals(patchname.replace("samples", "residuals"))
+    delta = r.make_exp(value="residual")
+    ierr = r.make_exp(value="ierr")
+    rfig, rax = pl.subplots()
+    cb = rax.imshow((delta * ierr).T, origin="lower")
+    rfig.colorbar(cb, label=r"$\chi=$ (Data - Model) / Unc")
+
+    val = s.get_sample_cat(-1)
+    return rfig, rax, val
+
+
+def make_catalog(root, n_sample=2058, n_full=0, bands=["F090W", "F200W", "F277W", "F356W"]):
+    avail = glob.glob(f"{root}/patches/*samples.h5")
+    pids = np.sort([int(os.path.basename(a).split("_")[0][5:]) for a in avail])
+
+    # Get catalog data type
+    shapes = Samples(f"{root}/patches/patch{pids[0]}_samples.h5").shape_cols
+    scols = bands + shapes
+    icols = [("id", "<i4"), ("wall", "<f4"), ("lnp", "<f8", n_sample)]
+
+    if n_full:
+        n_out = n_full
+    else:
+        n_out = len(pids)
+    new = np.dtype(icols + [(c, float, n_sample) for c in scols])
+    cat = np.zeros(n_out, new)
+
+    # Make and fill the catalog
+    cat["id"] = -1
+    for p in pids:
+        s = Samples(f"{root}/patches/patch{p}_samples.h5")
+        if s.n_sample != n_sample:
+            print(f"sizes do not match for patch {p}")
+            continue
+        cat["id"][p] = p
+        cat["wall"][p] = s.wall_time
+        cat["lnp"][p] = s.stats["model_logp"]
+        for col in shapes + s.bands:
+            cat[col][p] = s.chaincat[col][:, -n_sample:]
+
+    return cat
+
+
+def snr_plot(scat, truths, dx=0.05):
+
+    sel = scat["id"] >= 0
+    jitter = np.random.uniform(1 - dx, 1 + dx, len(truths))
+
+    sfig, sax = pl.subplots(figsize=fsize)
+    for b in bands:
+        flux = scat[sel][b]
+        sax.plot(truths[sel]["snr"] * jitter[sel],
+                 flux.mean(axis=-1) / flux.std(axis=-1),
+                 alpha=0.6, marker="o", linestyle="", label=b)
+    sax.set_xlabel("Input image S/N within rhalf")
+    sax.set_ylabel(r"$\langle f \rangle / \sigma_f$  from samples")
+    xx = np.linspace(0, truths["snr"].max(), 100)
+    sax.plot(xx, xx, linestyle=":", color="k")
+    sax.plot(xx, xx / 4, linestyle=":", color="k", label="SNR/4")
+
+    sax.set_xlim(truths["snr"].min()/2, truths["snr"].max()*1.5)
+    sax.set_ylim(truths["snr"].min()/5, truths["snr"].max()*1.5)
+    sax.set_xscale("log")
+    sax.set_yscale("log")
+
+    return sfig, sax
+
+
+def compare_parameters(scat, truths, parname, point_type="median",
+                       colorby="band", splitby="snr"):
+
+    splits = np.unique(truths[splitby])
+    colors = np.unique(truths[colorby])
+
+    yy = scat[parname]
+    y = np.percentile(yy, [16, 50, 84], axis=-1)
+    ind_ml = np.argmax(scat["lnp"], axis=-1)
+    ymap = yy[np.arange(len(yy)), ind_ml]
+    x = truths[parname].copy()
+    xr = x.min()*0.9, x.max()*1.1
+    dx = np.diff(xr)
+
+    jitter = np.random.uniform(-dx*0.02, dx*0.02, len(x))
+    x = x + jitter
+
+    dfig, daxes = pl.subplots(len(splits), 1, sharex=True, figsize=fsize)
+    for i, s in enumerate(splits):
+        ax = daxes[i]
+        for i, c in enumerate(colors):
+            sel = (scat["id"] >= 0) & (truths[splitby] == s) & (truths[colorby] == c)
+            if point_type == "median":
+                ax.plot(x[sel], y[1, sel], marker="o", linestyle="", alpha=0.5, label=f"{colorby}={c}")
+            else:
+                ax.plot(x[sel], ymap[sel], marker="o", linestyle="", alpha=0.5, label=f"{colorby}={c}")
+            ax.errorbar(x[sel], y[1, sel], np.diff(y, axis=0)[:, sel],
+                        marker="", linestyle="", color="gray")
+            line = np.linspace(*xr)
+            ax.plot(line, line, "k:")
+        ax.text(0.8, 0.2, f"{splitby.upper()}={s}", transform=ax.transAxes)
+    dfig.suptitle(parname)
+    daxes[-1].set_xlabel(f"{parname} (input)")
+    [ax.set_ylabel(f"{parname} (forcepho)") for ax in daxes]
+
+    return dfig, daxes
+
+
+def aperture_flux(rcat, truths):
+    band = truths["band"]
+    rhalf = truths["rhalf"]
+    for i, row in enumerate(rcat):
+        total_flux = row[band[i]]
+        # compute fraction of flux within true Re
+        fr = frac_sersic(rhalf[i], sersic=row["serisc"], rhalf=row["rhalf"])
+        apflux = total_flux * fr
+
+
+if __name__ == "__main__":
+
+    # bad: 806
+    # good: 836, 566
+    # middling: 802
+
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument("--root", type=str, default="output/test_sampling_v1")
+    parser.add_argument("--patch_index", type=int, default=-1)
+    parser.add_argument("--parname", type=str, default="",
+                        choices=["", "rhalf", "sersic", "q"])
+    parser.add_argument("--point_type", type=str, default="median",
+                        choices=["median", "map"])
+    args = parser.parse_args()
+
+    pl.ion()
+    truths = fits.getdata(f"{args.root}/test_grid.fits")
+
+    if args.patch_index > 0:
+        title_fmt = "band={band}, SNR={snr},\nnsersic={sersic:.1f}, rhalf={rhalf:.2f}, q={q:.2f}"
+        patchname = f"{args.root}/patches/patch{args.patch_index}_samples.h5"
+        truth_dict = {c: truths[args.patch_index][c] for c in truths.dtype.names}
+        title = title_fmt.format(**truth_dict)
+        tag = title.replace(",", "_").lower().replace(" ", "").replace("\n", "")
+        print(tag)
+
+        tfig, ax = plot_trace(patchname)
+        tfig.suptitle(title)
+        tfig.tight_layout()
+        tfig.savefig(f"figures/trace/{tag}_trace.png", dpi=200)
+        pl.close(tfig)
+
+        cfig, caxes = plot_corner(patchname)
+        cfig.text(0.4, 0.8, title, transform=cfig.transFigure)
+        cfig.savefig(f"figures/corner/{tag}_corner.png", dpi=200)
+        pl.close(cfig)
+
+        rfig, raxes, val = plot_residual(patchname)
+        vdict = deepcopy(truth_dict)
+        vdict.update({c: val[c][0] for c in val.dtype.names})
+        vtitle = "Last iteration:" + title_fmt.format(**vdict)
+        rfig.suptitle(vtitle)
+        rfig.savefig(f"figures/residuals/{tag}_residual.png", dpi=200)
+        pl.close(rfig)
+
+        sys.exit()
+
+    else:
+        bands = [str(b) for b in np.unique(truths["band"]).tolist()]
+        scat = make_catalog(args.root, n_full=len(truths), bands=bands)
+        fits.writeto(f"{args.root}/samples_catalog.fits", scat, overwrite=True)
+
+    if args.parname:
+        dfig, daxes = compare_parameters(scat, truths, args.parname,
+                                         point_type=args.point_type)
+        daxes[-1].legend(loc="upper left")
+        dfig.savefig(f"figures/compare_{args.parname}.png", dpi=300)
+
+    else:
+        sfig, sax = snr_plot(scat, truths)
+        sax.legend()
--- tests/galsim_tests/test_cannon.sh
+++ tests/galsim_tests/test_cannon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --mail-type=None               # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-type=None             # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --job-name=test-force        # Job name
 #SBATCH --partition=gpu_test         # queue for job submission
 #SBATCH --ntasks=1                   # Number of MPI ranks
@@ -8,17 +8,15 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem-per-cpu=1000
 #SBATCH --time=3:00:00               # Time limit hrs:min:sec
-#SBATCH --output=logs/forcetest_%A_%a.log    # Standard output and error log
-#SBATCH --error=logs/forcetest_%A_%a.log    # Standard output and error log
+#SBATCH --output=logs/ftest_%A_%a.log    # Standard output and error log
+#SBATCH --error=logs/ftest_%A_%a.log    # Standard output and error log
 pwd -P; hostname; date
 
 module purge
 module load git/2.17.0-fasrc01
 module load intel/19.0.5-fasrc01 openmpi/4.0.1-fasrc01 hdf5/1.10.5-fasrc01
 module load cuda/11.4.2-fasrc01     # HeLmod latest
-#module load cuda/10.1.243-fasrc01   # HeLmod
 module load Anaconda3/5.0.1-fasrc01 # HeLmod w/o hdf5, matplotlib
-#module load Anaconda3/5.0.1-fasrc02 # HeLmod w/ hdf5, matplotlib
 
 export PROJECT_DIR=$SCRATCH/eisenstein_lab/$USER/force-test
 cd $PROJECT_DIR
