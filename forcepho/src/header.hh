c1e67c21e5208cc01db4af592d7e91d6a3407d6b
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2022-02-01 17:27:37 -0500

    define storage variable.

--- forcepho/src/header.hh
+++ forcepho/src/header.hh
@@ -10,6 +10,13 @@
 #define CUDA_MEMBER
 #endif
 
+#ifdef __CUDACC__
+#define CUDA_CALLABLE_MEMBER __host__ __device__
+#else
+#define CUDA_CALLABLE_MEMBER
+#endif
+
+
 #include "kernel_limits.h"
 
 #include <stdio.h>
